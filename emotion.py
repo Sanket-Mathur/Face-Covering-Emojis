b2fc18a18d364deb6c04a4c8925707d9cdbfc9de
Sanket-Mathur <rajeev.sanket@gmail.com>
2021-03-09 23:06:37 +0530

    Emotion Detection Model v1

--- emotion.py
+++ emotion.py
@@ -1,47 +1,31 @@
 import os  
 import cv2  
 import numpy as np  
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D, BatchNormalization, AveragePooling2D
+from keras.models import model_from_json  
 from keras.preprocessing import image  
 
-model = Sequential()
-model.add(Conv2D(64, kernel_size=(3, 3), activation='relu', input_shape=(48, 48, 1)))
-model.add(Conv2D(64, kernel_size= (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2,2), strides=(2, 2)))
-model.add(Dropout(0.5))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2,2), strides=(2, 2)))
-model.add(Dropout(0.5))
-model.add(Conv2D(128, (3, 3), activation='relu'))
-model.add(Conv2D(128, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2,2), strides=(2, 2)))
-model.add(Flatten())
-model.add(Dense(1024, activation='relu'))
-model.add(Dropout(0.2))
-model.add(Dense(1024, activation='relu'))
-model.add(Dropout(0.2))
-model.add(Dense(7, activation='softmax'))
-model.load_weights('Models/emotionV1.h5')      
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+model = model_from_json(open("Models/fer.json", "r").read())  
+model.load_weights('Models/fer.h5')        
   
-face_haar_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')  
+face_haar_cascade = cv2.CascadeClassifier('Models/haarcascade_frontalface_default.xml')  
   
   
-cap=cv2.VideoCapture(0)  
+cap = cv2.VideoCapture(0)  
   
 while True:  
-    ret,test_img=cap.read()# captures frame and returns boolean value and captured image  
+    ret, test_img = cap.read()  
     if not ret:  
         continue  
-    gray_img= cv2.cvtColor(test_img, cv2.COLOR_BGR2GRAY)  
+    gray_img = cv2.cvtColor(test_img, cv2.COLOR_BGR2GRAY)  
   
     faces_detected = face_haar_cascade.detectMultiScale(gray_img, 1.32, 5)  
   
   
     for (x,y,w,h) in faces_detected:  
         cv2.rectangle(test_img,(x,y),(x+w,y+h),(255,0,0),thickness=7)  
-        roi_gray=gray_img[y:y+w,x:x+h]#cropping region of interest i.e. face area from  image  
+        roi_gray=gray_img[y:y+w,x:x+h]
         roi_gray=cv2.resize(roi_gray,(48,48))  
         img_pixels = image.img_to_array(roi_gray)  
         img_pixels = np.expand_dims(img_pixels, axis = 0)  
@@ -49,7 +33,6 @@ while True:
   
         predictions = model.predict(img_pixels)  
   
-        #find max indexed array  
         max_index = np.argmax(predictions[0])  
   
         emotions = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral')  
@@ -58,11 +41,11 @@ while True:
         cv2.putText(test_img, predicted_emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)  
   
     resized_img = cv2.resize(test_img, (1000, 700))  
-    cv2.imshow('Facial emotion analysis ',resized_img)  
+    cv2.imshow('Face Covering with Emotion Detection',resized_img)  
   
   
   
-    if cv2.waitKey(10) == ord('q'):#wait until 'q' key is pressed  
+    if cv2.waitKey(10) == ord('q'):
         break  
   
 cap.release()  
