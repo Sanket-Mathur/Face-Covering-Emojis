fda6e62c5045945cedfffd331349065bae64ec58
Sanket-Mathur <rajeev.sanket@gmail.com>
2021-03-10 23:17:34 +0530

    Changed the structure to Object Oriented

--- emotion.py
+++ emotion.py
@@ -4,49 +4,57 @@ import numpy as np
 from keras.models import model_from_json  
 from keras.preprocessing import image  
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1" # to ignore initialization of GPU
 
-model = model_from_json(open("Models/fer.json", "r").read())  
-model.load_weights('Models/fer.h5')        
-  
-face_haar_cascade = cv2.CascadeClassifier('Models/haarcascade_frontalface_default.xml')  
-  
-  
-cap = cv2.VideoCapture(0)  
-  
-while True:  
-    ret, test_img = cap.read()  
-    if not ret:  
-        continue  
-    gray_img = cv2.cvtColor(test_img, cv2.COLOR_BGR2GRAY)  
-  
-    faces_detected = face_haar_cascade.detectMultiScale(gray_img, 1.32, 5)  
-  
-  
-    for (x,y,w,h) in faces_detected:  
-        cv2.rectangle(test_img,(x,y),(x+w,y+h),(255,0,0),thickness=7)  
-        roi_gray=gray_img[y:y+w,x:x+h]
-        roi_gray=cv2.resize(roi_gray,(48,48))  
-        img_pixels = image.img_to_array(roi_gray)  
-        img_pixels = np.expand_dims(img_pixels, axis = 0)  
-        img_pixels /= 255  
-  
-        predictions = model.predict(img_pixels)  
-  
-        max_index = np.argmax(predictions[0])  
-  
-        emotions = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral')  
-        predicted_emotion = emotions[max_index]  
-  
-        cv2.putText(test_img, predicted_emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)  
-  
-    resized_img = cv2.resize(test_img, (1000, 700))  
-    cv2.imshow('Face Covering with Emotion Detection',resized_img)  
-  
-  
-  
-    if cv2.waitKey(10) == ord('q'):
-        break  
-  
-cap.release()  
-cv2.destroyAllWindows()
+class Emotions:
+    def __init__(self):
+        """ loading the models, weights and haarcascades and also occupying the input resource i.e the default camera """
+        self.model = model_from_json(open("Models/fer.json", 'r').read())
+        self.model.load_weights('Models/fer.h5')
+        self.lookup = ['angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral']
+
+        self.haar_cascade = cv2.CascadeClassifier('Models/haarcascade_frontalface_default.xml')
+        self.cap = cv2.VideoCapture(0)
+    
+    def run(self):
+        """ the main application loop of the software """
+        while True:
+            ret, img = self.cap.read()
+            if not ret:
+                continue
+
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  
+            faces_detected = self.haar_cascade.detectMultiScale(gray, 1.32, 5)
+
+            if len(faces_detected):
+                pred = self.predict(faces_detected, img, gray)
+                cv2.putText(img, self.lookup[pred], (20,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
+            
+            cv2.imshow('Face Covering with Emotion Detection', img)
+
+            if cv2.waitKey(10) == ord('q'):
+                break
+    
+    def predict(self, faces, img, gray):
+        """ Converts the image to an array and predicts the emotion """
+        for (x,y,w,h) in faces:  
+            cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), thickness=5)  
+            roi = gray[y:y+w, x:x+h]
+            roi = cv2.resize(roi,(48,48))  
+            img_arr = np.expand_dims(image.img_to_array(roi), axis = 0) / 255
+            
+            predictions = self.model.predict(img_arr)  
+            pred = np.argmax(predictions[0])
+            return pred            
+
+    def __del__(self):
+        """ destroying the window and releasing the occupied resources """
+        self.cap.release()
+
+def main():
+    o = Emotions()
+    o.run()
+    del o
+
+if __name__ == '__main__':
+    main()
