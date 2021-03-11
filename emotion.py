0172a3911977af543c3646834d5d7923f32fc800
Sanket-Mathur <rajeev.sanket@gmail.com>
2021-03-11 16:32:24 +0530

    Added prediction confidence bar

--- emotion.py
+++ emotion.py
@@ -9,13 +9,16 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "-1" # to ignore initialization of GPU
 
 class Emotions:
     def __init__(self):
-        """ loading the models, weights and haarcascades and also occupying the input resource i.e the default camera """
+        """ loading the models, weights and haarcascades; occupying the input resource i.e the default camera and some other configurations """
         self.model = model_from_json(open("Models/fer.json", 'r').read())
         self.model.load_weights('Models/fer.h5')
         self.lookup = ['angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral']
 
         self.haar_cascade = cv2.CascadeClassifier('Models/haarcascade_frontalface_default.xml')
         self.cap = cv2.VideoCapture(0)
+
+        self.emo_list = ['normal', 'cartoon', 'anime']
+        self.emo_style = 0
     
     def run(self):
         """ the main application loop of the software """
@@ -29,12 +32,19 @@ class Emotions:
 
             if len(faces_detected):
                 pred = self.predict(faces_detected, gray)
-                cv2.putText(self.img, self.lookup[pred], (20,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
+                cv2.putText(self.img, self.lookup[pred], (500,25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
+            else:
+                cv2.putText(self.img, 'NULL', (500,25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
+                self.place_predbar([0,0,0,0,0,0,0])
             
+            self.img = cv2.resize(self.img, (1000,700))
             cv2.imshow('Face Covering with Emotion Detection', self.img)
 
-            if cv2.waitKey(10) == ord('q'):
+            key = cv2.waitKey(10)
+            if key == ord('q'):
                 break
+            elif key == ord('c'):
+                self.emo_style = (self.emo_style+1) % 3
     
     def predict(self, faces, gray):
         """ Converts the image to an array and predicts the emotion """
@@ -46,6 +56,7 @@ class Emotions:
             predictions = self.model.predict(img_arr)  
             pred = np.argmax(predictions[0])
             self.place_emoji(pred, x, y, w, h)
+            self.place_predbar(predictions[0])
         return pred        
 
     def overlay_image_alpha(self, img, img_overlay, x, y, alpha_mask):
@@ -54,15 +65,12 @@ class Emotions:
         y1, y2 = max(0, y), min(img.shape[0], y + img_overlay.shape[0])
         x1, x2 = max(0, x), min(img.shape[1], x + img_overlay.shape[1])
 
-        # Overlay ranges
         y1o, y2o = max(0, -y), min(img_overlay.shape[0], img.shape[0] - y)
         x1o, x2o = max(0, -x), min(img_overlay.shape[1], img.shape[1] - x)
 
-        # Exit if nothing to do
         if y1 >= y2 or x1 >= x2 or y1o >= y2o or x1o >= x2o:
             return
 
-        # Blend overlay within the determined ranges
         img_crop = img[y1:y2, x1:x2]
         img_overlay_crop = img_overlay[y1o:y2o, x1o:x2o]
         alpha = alpha_mask[y1o:y2o, x1o:x2o, np.newaxis]
@@ -71,10 +79,11 @@ class Emotions:
         img_crop[:] = alpha * img_overlay_crop + alpha_inv * img_crop
 
     def place_emoji(self, pred, x, y, w, h):
+        """ overlaying the emoji on the image of the user in order to cover his identity """
         img_cvt = cv2.cvtColor(self.img, cv2.COLOR_BGR2RGB)
         img_pil = np.array(Image.fromarray(img_cvt))
 
-        emoji = np.array(Image.open('Emojis/normal/' + self.lookup[pred] +'.png').resize((w+40, h+40)))
+        emoji = np.array(Image.open('Emojis/' + self.emo_list[self.emo_style] + '/' + self.lookup[pred] +'.png').resize((w+40, h+40)))
 
         alpha_mask = emoji[:, :, 3] / 255.0
         img_result = img_pil[:, :, :3].copy()
@@ -82,6 +91,13 @@ class Emotions:
         self.overlay_image_alpha(img_result, img_overlay, x-20, y-20, alpha_mask)
 
         self.img = cv2.cvtColor(np.array(img_result), cv2.COLOR_RGB2BGR)
+
+    def place_predbar(self, predictions):
+        """ drawing the bar representing the prediction confidence given for each from """
+        for i in range(7):
+            cv2.putText(self.img, self.lookup[i], (10,20*(i+1)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 2)
+            cv2.rectangle(self.img, (80, 10+(20*i)), (180, 20*(i+1)), (0, 255, 0), 2)
+            cv2.rectangle(self.img, (80, 10+(20*i)), (int(80 + predictions[i]*100), 20*(i+1)), (0, 255, 0), -1)
 
     def __del__(self):
         """ destroying the window and releasing the occupied resources """
