57f80a2c28cb3d0da3462166f6152fce6d3320e6
Sanket-Mathur <rajeev.sanket@gmail.com>
2021-03-13 00:44:50 +0530

    Added face blur option

--- emotion.py
+++ emotion.py
@@ -17,7 +17,7 @@ class Emotions:
         self.haar_cascade = cv2.CascadeClassifier('Models/haarcascade_frontalface_default.xml')
         self.cap = cv2.VideoCapture(0)
 
-        self.emo_list = ['normal', 'cartoon', 'anime']
+        self.emo_list = ['blur', 'normal', 'cartoon', 'anime']
         self.emo_style = 0
     
     def run(self):
@@ -44,7 +44,7 @@ class Emotions:
             if key == ord('q'):
                 break
             elif key == ord('c'):
-                self.emo_style = (self.emo_style+1) % 3
+                self.emo_style = (self.emo_style+1) % 4
     
     def predict(self, faces, gray):
         """ Converts the image to an array and predicts the emotion """
@@ -60,8 +60,7 @@ class Emotions:
         return pred        
 
     def overlay_image_alpha(self, img, img_overlay, x, y, alpha_mask):
-        """Overlay `img_overlay` onto `img` at (x, y) and blend using `alpha_mask`.
-        `alpha_mask` must have same HxW as `img_overlay` and values in range [0, 1]."""
+        """ Overlay `img_overlay` onto `img` at (x, y) and blend using `alpha_mask`. `alpha_mask` must have same HxW as `img_overlay` and values in range [0, 1] """
         y1, y2 = max(0, y), min(img.shape[0], y + img_overlay.shape[0])
         x1, x2 = max(0, x), min(img.shape[1], x + img_overlay.shape[1])
 
@@ -80,17 +79,20 @@ class Emotions:
 
     def place_emoji(self, pred, x, y, w, h):
         """ overlaying the emoji on the image of the user in order to cover his identity """
-        img_cvt = cv2.cvtColor(self.img, cv2.COLOR_BGR2RGB)
-        img_pil = np.array(Image.fromarray(img_cvt))
+        if self.emo_style:
+            img_cvt = cv2.cvtColor(self.img, cv2.COLOR_BGR2RGB)
+            img_pil = np.array(Image.fromarray(img_cvt))
 
-        emoji = np.array(Image.open('Emojis/' + self.emo_list[self.emo_style] + '/' + self.lookup[pred] +'.png').resize((w+40, h+40)))
+            emoji = np.array(Image.open('Emojis/' + self.emo_list[self.emo_style] + '/' + self.lookup[pred] +'.png').resize((w+40, h+40)))
 
-        alpha_mask = emoji[:, :, 3] / 255.0
-        img_result = img_pil[:, :, :3].copy()
-        img_overlay = emoji[:, :, :3]
-        self.overlay_image_alpha(img_result, img_overlay, x-20, y-20, alpha_mask)
+            alpha_mask = emoji[:, :, 3] / 255.0
+            img_result = img_pil[:, :, :3].copy()
+            img_overlay = emoji[:, :, :3]
+            self.overlay_image_alpha(img_result, img_overlay, x-20, y-20, alpha_mask)
 
-        self.img = cv2.cvtColor(np.array(img_result), cv2.COLOR_RGB2BGR)
+            self.img = cv2.cvtColor(np.array(img_result), cv2.COLOR_RGB2BGR)
+        else:
+            self.img[y-20:y+h+20, x-20:x+w+20] = cv2.GaussianBlur(self.img[y-20:y+h+20, x-20:x+w+20], (151,151), 0)
 
     def place_predbar(self, predictions):
         """ drawing the bar representing the prediction confidence given for each from """
